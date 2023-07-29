c7e67a431b87ea0109f9a81973f99f77ea006163
1-Amin <azimovaminjon063@gmail.com>
2023-07-30 03:54:32 +0700

    First Commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+
+
+function Header() {
+const [ss, setss] = useState(false) 
+  return (
+    <div>
+
+    </div>  
+    
+  );
+  
+}
+
+export default Header;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Navbar(){
+    return (
+        <div>
+            <nav>
+                <img src="./images/logo.png"/>
+            </nav>
+        </div>
+    );
+}
+
+
+export default Navbar; 
